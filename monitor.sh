7e961c3bb703e85ba1f20cf4657bc4c62728a0b1
nobody <23215696+taicaile@users.noreply.github.com>
2021-11-16 23:35:13 +0800

    update

--- monitor.sh
+++ monitor.sh
@@ -4,7 +4,10 @@ set -e
 
 MONITOR_DIR=ready2download
 
-FILES=$(BaiduPCS-Go tree --depth 0 "$MONITOR_DIR" | cut -d " " -f 2)
-for FILE in $FILES; do
+FILES=$(BaiduPCS-Go tree --depth 0 "$MONITOR_DIR")
+IFS=$'\n'
+for LINE in $FILES; do
+    IFS=' ' read -r ID FILE <<< "$LINE"
     BaiduPCS-Go d "$MONITOR_DIR/$FILE" && BaiduPCS-Go rm "$MONITOR_DIR/$FILE"
+    IFS=$'\n'
 done
