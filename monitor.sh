bdea2f4b093dd884a84285d38a1a3c06798466bd
nobody <23215696+taicaile@users.noreply.github.com>
2021-11-16 22:55:31 +0800

    update

--- monitor.sh
+++ monitor.sh
@@ -1,10 +1,10 @@
-#/usr/bin/bash
+#!/usr/bin/bash
 
 set -e
 
 MONITOR_DIR=ready2download
 
-FILES=`BaiduPCS-Go tree --depth 0 "$MONITOR_DIR" | cut -d " " -f 2`
+FILES=$(BaiduPCS-Go tree --depth 0 "$MONITOR_DIR" | cut -d " " -f 2)
 for FILE in $FILES; do
     BaiduPCS-Go d "$MONITOR_DIR/$FILE" && BaiduPCS-Go rm "$MONITOR_DIR/$FILE"
 done
