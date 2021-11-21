7ae8c610fcaac549ca130e94220e3772b99f1b85
nobody <23215696+taicaile@users.noreply.github.com>
2021-11-21 16:56:27 +0800

    update

--- monitor_aliyun.sh
+++ monitor_aliyun.sh
@@ -21,15 +21,16 @@ if ! $COMMAND ls "$MONITOR_DIR" | grep -q "文件大小"; then
     exit 1
 fi
 
-FILES=$($COMMAND ls "$MONITOR_DIR" |  head -n -2 | tail -n +2)
+# FILES=$($COMMAND ls "$MONITOR_DIR" |  head -n -2 | tail -n +2)
+FILES=$($COMMAND ls "$MONITOR_DIR" | awk 'BEGIN{RS="\n" ;FS=" "; NF=5}/[0-9]{2}:[0-9]{2}:[0-9]{2}/{print $5}')
 
 IFS=$'\n'
-for LINE in $FILES; do
-    FILE="${LINE:36}"
-    FILE="${FILE%%+( )}"
+for FILE in $FILES; do
+    # FILE=$(echo $LINE | awk 'BEGIN{FS=" "}{print $5}')
+    # FILE="${FILE%%+( )}"
     # echo "${FILE%%/*}"
     if [ -n "$FILE" ] && [ "$FILE" != " " ];then
-        echo "[Download]: $MONITOR_DIR/$FILE"
-        $COMMAND d "$MONITOR_DIR/$FILE" && $COMMAND rm "$MONITOR_DIR/$FILE"
+        echo "[Download]: $MONITOR_DIR/${FILE%%+([ \t])}"
+        $COMMAND d "$MONITOR_DIR/${FILE%%+( )}" && $COMMAND rm "$MONITOR_DIR/${FILE%%+( )}"
     fi
 done
