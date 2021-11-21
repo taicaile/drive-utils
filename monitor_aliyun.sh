be549fe895396210292dae1d8cb91490f1fa8f29
nobody <23215696+taicaile@users.noreply.github.com>
2021-11-21 18:32:46 +0800

    update

--- monitor_aliyun.sh
+++ monitor_aliyun.sh
@@ -21,16 +21,22 @@ if ! $COMMAND ls "$MONITOR_DIR" | grep -q "文件大小"; then
     exit 1
 fi
 
-# FILES=$($COMMAND ls "$MONITOR_DIR" |  head -n -2 | tail -n +2)
-FILES=$($COMMAND ls "$MONITOR_DIR" | awk 'BEGIN{RS="\n" ;FS=" "; NF=5}/[0-9]{2}:[0-9]{2}:[0-9]{2}/{print $5}')
+while :
+do
+    # FILES=$($COMMAND ls "$MONITOR_DIR" |  head -n -2 | tail -n +2)
+    FILES=$($COMMAND ls "$MONITOR_DIR" | awk 'BEGIN{RS="\n" ;FS=" "; NF=5}/[0-9]{2}:[0-9]{2}:[0-9]{2}/{print $5}')
 
-IFS=$'\n'
-for FILE in $FILES; do
-    # FILE=$(echo $LINE | awk 'BEGIN{FS=" "}{print $5}')
-    # FILE="${FILE%%+( )}"
-    # echo "${FILE%%/*}"
-    if [ -n "$FILE" ] && [ "$FILE" != " " ];then
-        echo "[Download]: $MONITOR_DIR/${FILE}"
-        $COMMAND d "$MONITOR_DIR/${FILE}" && $COMMAND rm "$MONITOR_DIR/${FILE}"
-    fi
+    IFS=$'\n'
+    for FILE in $FILES; do
+        # FILE=$(echo $LINE | awk 'BEGIN{FS=" "}{print $5}')
+        # FILE="${FILE%%+( )}"
+        # echo "${FILE%%/*}"
+        if [ -n "$FILE" ] && [ "$FILE" != " " ];then
+            echo "[Download]: $MONITOR_DIR/${FILE}"
+            $COMMAND d "$MONITOR_DIR/${FILE}" && $COMMAND rm "$MONITOR_DIR/${FILE}"
+        fi
+    done
+
+    sleep $(( (RANDOM % 5)  + 5 ))h
+
 done
